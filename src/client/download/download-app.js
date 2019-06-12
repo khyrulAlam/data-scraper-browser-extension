90aff70f8007f6a3603370f1c576afc01d25c7cb
khyrulAlam <khyrulalam69@gmail.com>
2019-06-12 23:53:04 +0600

    download page basic design

--- src/client/download/download-app.js
+++ src/client/download/download-app.js
@@ -1,18 +1,19 @@
+import "./style.css";
 console.log(" 🍌 ");
-chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-  console.log(tabs);
-});
+// chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+//   console.log(tabs);
+// });
 
-let senderId = "";
+// let senderId = "";
 
-chrome.runtime.onMessage.addListener(function(message, sender) {
-  console.log(message, sender);
-  senderId = message.senderId;
-});
+// chrome.runtime.onMessage.addListener(function(message, sender) {
+//   console.log(message, sender);
+//   senderId = message.senderId;
+// });
 
-document.querySelector("#send").addEventListener("click", function() {
-  if (!senderId) {
-    window.close();
-  }
-  chrome.tabs.sendMessage(senderId, { text: "message from other page" });
-});
+// document.querySelector("#send").addEventListener("click", function() {
+//   if (!senderId) {
+//     window.close();
+//   }
+//   chrome.tabs.sendMessage(senderId, { text: "message from other page" });
+// });
